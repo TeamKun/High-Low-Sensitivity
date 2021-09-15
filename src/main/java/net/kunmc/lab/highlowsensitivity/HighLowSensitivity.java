cad0025005dd18f64ee913d2a3aab3514f525a73
MORIMORI0317 <morimori.0317@outlook.jp>
2021-09-16 05:00:57 +0900

    first

---
+++ src/main/java/net/kunmc/lab/highlowsensitivity/HighLowSensitivity.java
@@ -0,0 +1,12 @@
+package net.kunmc.lab.highlowsensitivity;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(HighLowSensitivity.MODID)
+public class HighLowSensitivity {
+    public static final String MODID = "highlowsensitivity";
+
+    public HighLowSensitivity() {
+
+    }
+}
